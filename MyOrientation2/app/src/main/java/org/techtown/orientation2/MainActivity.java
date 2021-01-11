a956e2a8f0c57a975727be89857448453c0f3823
zole77 <qjatn8595@gmail.com>
2021-01-11 12:01:28 +0900

    android study Orientation ++

---
+++ MyOrientation2/app/src/main/java/org/techtown/orientation2/MainActivity.java
@@ -0,0 +1,33 @@
+package org.techtown.orientation2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            showToast("가로 방향임");
+        }else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            showToast("세로 방향임");
+        }
+    }
+
+    public void showToast(String data){
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+    }
+}
