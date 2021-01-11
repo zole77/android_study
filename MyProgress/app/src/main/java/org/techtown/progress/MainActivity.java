d62e69f166d72a3a92000b07f506aebfff7272e3
zole77 <qjatn8595@gmail.com>
2021-01-11 12:03:58 +0900

    android study ProgressBar

---
+++ MyProgress/app/src/main/java/org/techtown/progress/MainActivity.java
@@ -0,0 +1,39 @@
+package org.techtown.progress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    int value = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value += 10;
+                if(value > 100){
+                    value = 0;
+                }
+
+                progressBar.setProgress(value);
+            }
+        });
+    }
+
+
+
+
+}
