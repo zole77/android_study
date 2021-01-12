d2044f0fc085ea6ad19680c515c6933689373e5b
zole77 <qjatn8595@gmail.com>
2021-01-12 12:15:02 +0900

    android study Intent 메뉴화면과 메인화면 상호작용

---
+++ MyIntent/app/src/main/java/org/techtown/intent/MainActivity.java
@@ -0,0 +1,42 @@
+package org.techtown.intent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivityForResult(intent, 101);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101){
+            if (data != null){
+                String name = data.getStringExtra("name");
+                if(name != null){
+                    Toast.makeText(this, "응답으로 받은 데이터: " + name, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+}
--- MyIntent/app/src/main/java/org/techtown/intent/MenuActivity.java
+++ MyIntent/app/src/main/java/org/techtown/intent/MenuActivity.java
@@ -2,7 +2,10 @@ package org.techtown.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("name", "mike");
+                setResult(RESULT_OK, intent);
+
+
+                finish();
+            }
+        });
+
     }
 }
