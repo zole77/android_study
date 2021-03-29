0d6097d05236250ce0b3e3193749784e0d98fc0f
Park BeomSu <qjatn8595@gmail.com>
2021-03-29 17:40:47 +0900

    SQLite를 이용한 예제, 기존 코드인 Intent를 띄워 수정, 삭제 하는 방식 말고 다이얼로그를 띄워서 가능하게끔 수정중

--- SQLiteExample2/app/src/main/java/org/techtown/sqliteexample2/ListDataActivity.java
+++ SQLiteExample2/app/src/main/java/org/techtown/sqliteexample2/ListDataActivity.java
@@ -1,13 +1,22 @@
 package org.techtown.sqliteexample2;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -20,6 +29,8 @@ public class ListDataActivity extends AppCompatActivity {
     DatabaseHelper mDatabaseHelper;
     private ListView mListView;
 
+    Button btn_dialog;
+    TextView tv_result;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,6 +54,52 @@ public class ListDataActivity extends AppCompatActivity {
 
         ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
         mListView.setAdapter(adapter);
+
+        //set an onItemClickListener to the ListView
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = adapterView.getItemAtPosition(i).toString();
+                Log.d(TAG, "onItemClick: You Clicked on " + name);
+
+                Cursor data = mDatabaseHelper.getItemID(name); //get the id associated with that name
+                int itemID = -1;
+                while(data.moveToNext()){
+                    itemID = data.getInt(0);
+                }
+                if(itemID > -1){
+                    Log.d(TAG, "onItemClick: The ID is: " + itemID);
+                    AlertDialog.Builder ad = new AlertDialog.Builder(ListDataActivity.this);
+                    ad.setIcon(R.mipmap.ic_launcher_round);
+                    ad.setTitle("제목");
+                    ad.setMessage("삭제하시겠습니까?");
+
+                    final EditText et = new EditText(ListDataActivity.this);
+                    ad.setView(et);
+
+                    ad.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    ad.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    ad.show();
+                }
+                else{
+                    toastMessage("No ID associated with that name");
+                }
+            }
+        });
     }
 
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
 }
