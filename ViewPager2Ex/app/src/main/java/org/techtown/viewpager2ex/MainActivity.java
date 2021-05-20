a77491afc9f048dac44a86145b633bea41c4cd93
Park BeomSu <qjatn8595@gmail.com>
2021-05-20 19:19:41 +0900

    뷰페이저2 주석으로 재해석해봄. 가이드 이 기능으로 만들것.

--- ViewPager2Ex/app/src/main/java/org/techtown/viewpager2ex/MainActivity.java
+++ ViewPager2Ex/app/src/main/java/org/techtown/viewpager2ex/MainActivity.java
@@ -33,16 +33,21 @@ public class MainActivity extends AppCompatActivity {
         text_view = findViewById(R.id.text_view);
 
         userList = new ArrayList<>();
+        //유저리스트에 들어갈 요소, icon, name, age 저장
         userList.add(new User(R.drawable.ic_baseline, "Kim", "22"));
         userList.add(new User(R.drawable.ic_android, "Lee", "23"));
         userList.add(new User(R.drawable.ic_bedtime, "Park", "26"));
 
+        //유저어댑터에 Context와 유저리스트 전달
         userAdapter = new UserAdapter(this, userList);
+        //뷰페이저2에 어댑터 연결
         viewPager2.setAdapter(userAdapter);
 
         materialButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 다음버튼을 누를때마다 뷰페이저 아이템 카운트가 하나씩 증가.
+                // 이 값이 어댑터가 전달받은 리스트의 사이즈보다 작으면
                 if(viewPager2.getCurrentItem() + 1 < userAdapter.getItemCount()){
                     viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
                 }else{
