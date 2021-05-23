264b7becc6453f26d0bd4dbeac95a783c778030f
Park BeomSu <qjatn8595@gmail.com>
2021-05-23 20:46:08 +0900

    첫 실행 시 가이드를 뷰페이저2로 실행하는 코드

--- ViewPager2Guide/app/src/main/java/org/techtown/viewpager2guide/MainActivity.java
+++ ViewPager2Guide/app/src/main/java/org/techtown/viewpager2guide/MainActivity.java
@@ -2,13 +2,29 @@ package org.techtown.viewpager2guide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public SharedPreferences prefs; // 선언하기
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prefs = getSharedPreferences("Pref", MODE_PRIVATE);
+        checkFirstRun();
+    }
+
+    public void checkFirstRun() {
+        boolean isFirstRun = prefs.getBoolean("isFirstRun", true);
+        if (isFirstRun) {
+            Intent intent = new Intent(getApplicationContext(), GuideActivity.class);
+            startActivity(intent);
+            prefs.edit().putBoolean("isFirstRun", false).apply();
+        }
+
     }
 }
